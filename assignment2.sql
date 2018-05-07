0452cd0bebab8e6be15fa666fd96f421abffd2f5
davidkevork <davidkevork@hotmail.com>
2018-05-07 16:22:37 +1000

    Update SQL statement

--- assignment2.sql
+++ assignment2.sql
@@ -37,6 +37,11 @@ Customer_Booking ( Customer_ID, TD_ID );
 FK: Customer_ID References Customer
 FK: TD_ID References Tour_Date
 
+Customer_Payment (PAYMENT_ID, BOOKING_ID, CUSTOMER_ID, PAYMENT_TYPE)
+PK : PAYMENT_ID Starts at 1 IDENTITY then INCREMENTS by 1.
+FK : BOOKING_ID References Customer_Booking
+FK : CUSTOMER_ID References Customer
+
 Part 3
 
 
@@ -51,6 +56,7 @@ DROP TABLE Tour_Date_Experts;
 DROP TABLE Wine_Experts;
 DROP TABLE Tour_Date;
 DROP TABLE Wine_Tour;
+DROP TABLE Customer_Payment;
 
 PROMPT *** CREATE TABLE Wine_Tour ***
 CREATE TABLE Wine_Tour (
@@ -62,8 +68,9 @@ CREATE TABLE Wine_Tour (
 );
 
 PROMPT *** CREATE TABLE Tour_Date ***
+/* GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1) */
 CREATE TABLE Tour_Date (
-	TD_ID NUMBER GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1),
+	TD_ID NUMBER,
 	T_ID VARCHAR(2),
 	TD_Date DATE,
 	TD_Price NUMBER,
@@ -116,7 +123,7 @@ CREATE TABLE Gift (
 	PRIMARY KEY (GIFT_ID)
 );
 
-PROMT *** CREATE TABLE Tour_Date_Gift ***
+PROMPT *** CREATE TABLE Tour_Date_Gift ***
 CREATE TABLE Tour_Date_Gift (
 	TD_ID NUMBER,
 	GIFT_ID NUMBER,
@@ -129,22 +136,31 @@ CREATE TABLE Customer_Gift (
 	GIFT_ID NUMBER,
 	TD_ID NUMBER,
 	CUSTOMER_ID VARCHAR2(4),
-FOREIGN KEY (GIFT_ID) REFERENCES Gift,
+	FOREIGN KEY (GIFT_ID) REFERENCES Gift,
 	FOREIGN KEY (TD_ID) REFERENCES Tour_Date,
 	FOREIGN KEY (CUSTOMER_ID) REFERENCES Customer
 );
 
 PROMPT *** CREATE TABLE Customer_Booking ***
 CREATE TABLE Customer_Booking (
+	BOOKING_ID NUMBER,
 	CUSTOMER_ID VARCHAR2(4),
 	TD_ID NUMBER,
+	PRIMARY KEY (BOOKING_ID),
 	FOREIGN KEY (CUSTOMER_ID) REFERENCES Customer,
 	FOREIGN KEY (TD_ID) REFERENCES Tour_Date
 );
 
-
-
-
+PROMPT *** CREATE TABLE Customer_Payment
+CREATE TABLE Customer_Payment (
+	PAYMENT_ID NUMBER GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1),
+	BOOKING_ID NUMBER,
+	CUSTOMER_ID  VARCHAR2(4),
+	PAYMENT_TYPE VARCHAR2(6),
+	PRIMARY KEY (PAYMENT_ID),
+	FOREIGN KEY (BOOKING_ID) REFERENCES Customer_Booking,
+	FOREIGN KEY (CUSTOMER_ID) REFERENCES Customer
+)
 
 PROMPT *** INSERT QUERIES ***
 
@@ -155,21 +171,23 @@ INSERT INTO Wine_Tour (T_ID, T_Name, T_Description, T_Current_Price) VALUES ('W1
 INSERT INTO Wine_Tour (T_ID, T_Name, T_Description, T_Current_Price) VALUES ('S1', 'Sparkling Wine Tour', 'The region is famed for its interesting forms of sparkling wines. Our expert tour leader will ensure that this is an experience never to be forgotten', 250);
 
 PROMPT *** TOUR_DATE ***
-INSERT INTO Tour_Date (TD_ID, T_ID, TD_Date, TD_Price) VALUES (1, 'R2', DATE('2017/10/07'), 170);
-INSERT INTO Tour_Date (TD_ID, T_ID, TD_Date, TD_Price) VALUES (2, 'B1', DATE('2017/11/11'), 270);
-INSERT INTO Tour_Date (TD_ID, T_ID, TD_Date, TD_Price) VALUES (3, 'B1', DATE('2017/02/18'), 280);
-INSERT INTO Tour_Date (TD_ID, T_ID, TD_Date, TD_Price) VALUES (4, 'R2', DATE('2017/02/18'), 190);
-INSERT INTO Tour_Date (TD_ID, T_ID, TD_Date, TD_Price) VALUES (5, 'R2', DATE('2017/03/07'), 200);
-INSERT INTO Tour_Date (TD_ID, T_ID, TD_Date, TD_Price) VALUES (6, 'B1', DATE('2017/03/14'), 300);
-INSERT INTO Tour_Date (TD_ID, T_ID, TD_Date, TD_Price) VALUES (7, 'W1', DATE('2017/06/03'), 200);
-INSERT INTO Tour_Date (TD_ID, T_ID, TD_Date, TD_Price) VALUES (8, 'S1', DATE('2017/10/27'), 250);
-INSERT INTO Tour_Date (TD_ID, T_ID, TD_Date, TD_Price) VALUES (9, 'W1', DATE('2017/10/29'), 220);
+INSERT INTO Tour_Date (TD_ID, T_ID, TD_Date, TD_Price) VALUES (1, 'R2', TO_DATE('2017/10/07', 'yyyy/mm/dd'), 170);
+INSERT INTO Tour_Date (TD_ID, T_ID, TD_Date, TD_Price) VALUES (2, 'B1', TO_DATE('2017/11/11', 'yyyy/mm/dd'), 270);
+INSERT INTO Tour_Date (TD_ID, T_ID, TD_Date, TD_Price) VALUES (3, 'B1', TO_DATE('2017/02/18', 'yyyy/mm/dd'), 280);
+INSERT INTO Tour_Date (TD_ID, T_ID, TD_Date, TD_Price) VALUES (4, 'R2', TO_DATE('2017/02/18', 'yyyy/mm/dd'), 190);
+INSERT INTO Tour_Date (TD_ID, T_ID, TD_Date, TD_Price) VALUES (5, 'R2', TO_DATE('2017/03/07', 'yyyy/mm/dd'), 200);
+INSERT INTO Tour_Date (TD_ID, T_ID, TD_Date, TD_Price) VALUES (6, 'B1', TO_DATE('2017/03/14', 'yyyy/mm/dd'), 300);
+INSERT INTO Tour_Date (TD_ID, T_ID, TD_Date, TD_Price) VALUES (7, 'W1', TO_DATE('2017/06/03', 'yyyy/mm/dd'), 200);
+INSERT INTO Tour_Date (TD_ID, T_ID, TD_Date, TD_Price) VALUES (8, 'S1', TO_DATE('2017/10/27', 'yyyy/mm/dd'), 250);
+INSERT INTO Tour_Date (TD_ID, T_ID, TD_Date, TD_Price) VALUES (9, 'W1', TO_DATE('2017/10/29', 'yyyy/mm/dd'), 220);
+
 
 PROMPT *** WINE_EXPERTS ***
 INSERT INTO Wine_Experts (Expert_ID, Expert_First_Name, Expert_Last_Name, Expert_Phone_Number) VALUES (5, 'Sue', 'Davies', 92142222);
-INSERT INTO Wine_Experts (Expert_ID, Expert_First_Name, Expert_Last_Name, Expert_Phone_Number) VALUES (1, 'Geoff, 'Millar', 92141111);
+INSERT INTO Wine_Experts (Expert_ID, Expert_First_Name, Expert_Last_Name, Expert_Phone_Number) VALUES (1, 'Geoff', 'Millar', 92141111);
 INSERT INTO Wine_Experts (Expert_ID, Expert_First_Name, Expert_Last_Name, Expert_Phone_Number) VALUES (8, 'Hillary', 'Cork', 92144444);
 INSERT INTO Wine_Experts (Expert_ID, Expert_First_Name, Expert_Last_Name, Expert_Phone_Number) VALUES (12, 'Penny', 'Folds', 92143333);
+
 
 PROMPT *** TOUR_DATE_EXPERTS ***
 INSERT INTO Tour_Date_Experts (TD_ID, Expert_ID) VALUES (1, 12);
@@ -180,3 +198,19 @@ INSERT INTO Tour_Date_Experts (TD_ID, Expert_ID) VALUES (3, 5);
 INSERT INTO Tour_Date_Experts (TD_ID, Expert_ID) VALUES (4, 12);
 INSERT INTO Tour_Date_Experts (TD_ID, Expert_ID) VALUES (5, 1);
 INSERT INTO Tour_Date_Experts (TD_ID, Expert_ID) VALUES (6, 12);
+INSERT INTO Tour_Date_Experts (TD_ID, Expert_ID) VALUES (6, 8);
+INSERT INTO Tour_Date_Experts (TD_ID, Expert_ID) VALUES (7, 5);
+INSERT INTO Tour_Date_Experts (TD_ID, Expert_ID) VALUES (8, 5);
+INSERT INTO Tour_Date_Experts (TD_ID, Expert_ID) VALUES (9, 8);
+INSERT INTO Tour_Date_Experts (TD_ID, Expert_ID) VALUES (9, 12);
+
+PROMPT *** Customer ***
+INSERT INTO Customer (Customer_ID, Customer_First_Name, Customer_Last_Name, Custoemr_Address, Customer_Phone) VALUES ('C123', 'Joel', 'Warren', '7 Bluff Rd', 92142277);
+
+/* CONSTRAINT AU_AUTHOR UNIQUE (sname, fname) */
+
+PROMPT *** Part 4 Invalid insert statements A test of Validation Rules With a forword by D.Trump***
+
+INSERT INTO Tour_Date_Experts (TD_ID, Expert_ID) VALUES ('B1', 22);
+INSERT INTO Customer_Booking (CUSTOMER_ID, TD_ID) VALUES ('C078','W1');
+INSERT INTO Customer_Payment (BOOKING_ID, CUSTOMER_ID, PAYMENT_TYPE) VALUES ('W1', 'C225', '');
